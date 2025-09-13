c968e28ebef8455eb0f9d44f35030293cb2d95f3
Divansir-Junior <jscrobut989@gmail.com>
2025-09-12 22:11:59 -0300

    (FIX) Models path changed

--- SistemaSolarLowPoly/src/script/planets.js
+++ SistemaSolarLowPoly/src/script/planets.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 const loader = new GLTFLoader();
 
 export function createSun (scene) {
-    loader.load('src/assets/models/Sun.glb', (gltf) => {
+    loader.load('models/Sun.glb', (gltf) => {
         const sun = gltf.scene;
         sun.scale.set(20,20,20); // Tamanho do Sol
         sun.position.set(0,0,0); // Posição 
