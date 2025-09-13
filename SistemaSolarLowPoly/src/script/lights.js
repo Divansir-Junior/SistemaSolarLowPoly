bc62a5001c09515ffc2db43c29e4cbf0a2c154a1
Divansir-Junior <jscrobut989@gmail.com>
2025-09-12 21:00:56 -0300

    (REMOVED) Function of  sun light

--- SistemaSolarLowPoly/src/script/lights.js
+++ SistemaSolarLowPoly/src/script/lights.js
@@ -1,12 +1,14 @@
 import * as THREE from 'three';
 
 export function createLights(scene) {
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 2);
   scene.add(ambientLight);
 
-  const sunLight = new THREE.PointLight(0xffffaa, 3, 2000);
+  const sunLight = new THREE.PointLight(0xffffaa, 3, 200);
   sunLight.position.set(0, 0, 0);
   scene.add(sunLight);
 
   return { ambientLight, sunLight };
+ 
 }
+
